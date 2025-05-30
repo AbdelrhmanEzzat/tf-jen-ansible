c6b3e98e9be8b50cc58753b0c9b21279dfa88ea7
AbdelrhmanEzzat <abdoezzat222@mail.com>
2025-05-30 10:56:35 +0300

    up

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "web" {
+resource "aws_instance" "demo" {
   ami           = "ami-0953476d60561c955"  # Ubuntu 20.04 in us-east-1
   instance_type = "t2.micro"
   key_name      = "tf-jen"
